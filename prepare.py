9a2098f838d73507ce4b6f95cd4de696dcf491cb
malachi-hale <malachi.d.hale@gmail.com>
2021-08-13 14:38:51 -0500

    classification exercises

--- prepare.py
+++ prepare.py
@@ -1,10 +1,12 @@
-def clean_data(df):
+import pandas as pd
+import os
+import env
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def prep_iris(df):
     df.drop_duplicates(inplace=True)
     df.drop(columns=['species_id'], inplace=True)
     df = df.rename(columns={"species_name": "species"})
     dummy_df = pd.get_dummies(df[['species']], drop_first=True)
     return pd.concat([df, dummy_df], axis=1)
-
-def prep_iris(df):
-    df = clean_data(df)
-    return df
