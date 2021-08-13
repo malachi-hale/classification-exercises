a02bd4c0c2056b83a792c79e1731710e45f2e7e1
malachi-hale <malachi.d.hale@gmail.com>
2021-08-13 12:31:59 -0500

    updated to exercises

--- acquire.py
+++ acquire.py
@@ -8,12 +8,13 @@ def get_connection(db, user=env.user, host=env.host, password=env.password):
 
 def get_titanic_data():
     filename = "titanic.csv"
-
+    sql = '''SELECT passenger_id, survived, pclass, sex, age, sibsp, parch, fare, embarked, class, deck, embark_town, alone 
+         FROM passengers'''
     if os.path.isfile(filename):
         return pd.read_csv(filename)
     else:
         # read the SQL query into a dataframe
-        df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+        df = pd.read_sql(sql, get_connection('titanic_db'))
 
         df.to_csv(filename)
         
@@ -23,12 +24,14 @@ def get_titanic_data():
     
 def get_iris_data():
     filename = "iris.csv"
-
+    sql = '''SELECT species.species_id, species_name, sepal_length, sepal_width, petal_length, petal_width 
+        FROM species 
+        JOIN measurements ON species.species_id = measurements.species_id'''
     if os.path.isfile(filename):
         return pd.read_csv(filename)
     else:
         # read the SQL query into a dataframe
-        df = pd.read_sql('SELECT species.species_id, species_name, sepal_length, sepal_width, petal_length, petal_width FROM species JOIN measurements ON species.species_id = measurements.species_id', get_connection('iris_db'))
+        df = pd.read_sql(sql , get_connection('iris_db'))
     
         # Return the dataframe to the calling code
         return df  
