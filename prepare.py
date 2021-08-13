6732fb09c4b75e353f88732642bf76e1ae6cad6e
malachi-hale <malachi.d.hale@gmail.com>
2021-08-13 16:43:41 -0500

    updates to classification exercises

--- prepare.py
+++ prepare.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def prep_iris(df):
     df.drop_duplicates(inplace=True)
-    df.drop(columns=['species_id'], inplace=True)
+    df = df.drop(columns='species_id')
     df = df.rename(columns={"species_name": "species"})
     dummy_df = pd.get_dummies(df[['species']], drop_first=True)
     return pd.concat([df, dummy_df], axis=1)
